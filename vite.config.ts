ecc4c37749e9fe0810349ce1e07415d351bf1021
Milos Hegr <milos.hegr@lifeliqe.com>
2025-04-26 13:33:38 +0200

    feat: init shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import mkcert from 'vite-plugin-mkcert';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +10,10 @@ export default defineConfig({
 	server: {
 		port: 3000,
 		proxy: {}
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src')
+		}
 	}
 });
