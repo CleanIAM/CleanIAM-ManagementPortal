969977ae27c19a8969ac4619f62f9a97f4a2031a
Milos Hegr <milos.hegr@lifeliqe.com>
2025-04-26 11:41:21 +0200

    feat: update orval generation

--- orval.config.ts
+++ orval.config.ts
@@ -3,9 +3,11 @@ import { defineConfig } from 'orval';
 export default defineConfig({
 	api: {
 		output: {
-			mode: 'single', // or 'single'
+			mode: 'tags-split', // or 'single'
 			target: 'src/api/generated/',
 			client: 'react-query', // THIS is for TanStack Query v4/v5
+			httpClient: 'fetch',
+			prettier: true,
 			override: {
 				mutator: {
 					path: 'src/api/custom-fetch.ts',
