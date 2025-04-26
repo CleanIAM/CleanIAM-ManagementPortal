04f5456c961b12d030915ef466ac59f46a8346ba
Milos Hegr <milos.hegr@lifeliqe.com>
2025-04-26 14:58:57 +0200

    feat: add react-toastify

--- src/Components/test.tsx
+++ src/Components/test.tsx
@@ -1,4 +1,6 @@
+import { toast } from 'react-toastify';
 import { useGetTest } from '../lib/api/generated/applications-api/applications-api';
+import { Button } from './ui/button';
 
 export const Test = () => {
 	const { data, isLoading } = useGetTest();
@@ -7,6 +9,8 @@ export const Test = () => {
 		<h1 className="text-2xl text-green-800">
 			{isLoading ? 'Loading...' : data?.data}
 			<br />
+
+			<Button onClick={() => toast('Hello toast')}> Toast</Button>
 		</h1>
 	);
 };
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { router } from './router';
 import './index.css';
-
+import { ToastContainer } from 'react-toastify';
 // Create a client
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -21,5 +21,16 @@ createRoot(document.getElementById('root')!).render(
 		<QueryClientProvider client={queryClient}>
 			<RouterProvider router={router} />
 		</QueryClientProvider>
+		<ToastContainer
+			position="bottom-right"
+			autoClose={5000}
+			hideProgressBar={false}
+			newestOnTop={false}
+			closeOnClick={false}
+			rtl={false}
+			pauseOnFocusLoss
+			pauseOnHover
+			theme="colored"
+		/>
 	</StrictMode>
 );
