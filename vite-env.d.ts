4bfaeb51fe3be22240d1f0560f4736b4c4277eb0
Milos Hegr <milos.hegr@lifeliqe.com>
2025-05-12 22:00:57 +0200

    fix: fix css update error

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -5,3 +5,9 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
+
+// Allow importing CSS modules
+declare module '*.css';
+declare module '*.scss';
+declare module '*.less';
+declare module '*.sass';
